588393f16d129ced756fa26829e0dcecc8e4ed0a
Rismaaaaaaaa <152695838+Rismaaaaaaaa@users.noreply.github.com>
2024-05-07 13:37:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function encrypt() {
         return;
     }
 
-    var ciphertext = RC4.encrypt(plaintext, key);
+    var ciphertext = CryptoJS.AES.encrypt(plaintext, key);
 
     document.getElementById('ciphertext').value = ciphertext;
 }
@@ -22,7 +22,7 @@ function decrypt() {
     }
 
     try {
-        var plaintext = RC4.decrypt(ciphertext, key);
+        var plaintext = CryptoJS.AES.decrypt(ciphertext, key);
         document.getElementById('plaintext').value = plaintext;
     } catch (e) {
         alert("Dekripsi gagal. Pastikan ciphertext dan kunci benar.");
